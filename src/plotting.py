81e5e9799ff98d8696ee4a10711a6883e301e6cf
hazrmard <ibrahim78786@gmail.com>
2019-04-09 17:12:32 -0500

    animated plotting fixes - multiple linestyles

--- src/plotting.py
+++ src/plotting.py
@@ -2,7 +2,8 @@
 Specialized plot operations for data frames.
 """
 
-from typing import Iterable, Tuple, Dict, Any
+from typing import Iterable, Tuple, Dict, Any, Union
+from itertools import zip_longest
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -13,6 +14,7 @@ def animate_dataframes(frames: Iterable[pd.DataFrame], ax: plt.Axes,
                        xlim: Tuple[float]=None, ylim: Tuple[float]=None,
                        xlabel: str='', ylabel: str='', labels: Iterable[str]=None,
                        xscale: str='linear', yscale: str='linear', legend: bool=True,
+                       fmt: Union[Iterable[Iterable[str]], Iterable, str]=(('-',), ('--',)),
                        anim_args: Dict[str, Any]={}) \
                        -> FuncAnimation:
     """
@@ -30,6 +32,9 @@ def animate_dataframes(frames: Iterable[pd.DataFrame], ax: plt.Axes,
     * `xscale, yscale`: Axis scales ('log' etc.). If `rseries` provided, yscale
     must be a tuple of two strings,
     * `legend`: Whether to show legend on plot.
+    * `fmt`: A format string for lines. Either a single string for all lines, or
+    an iterable of strings applying to lseries and rseries in order, or an iterable
+    of iterables of strings - one iterable for each lseries and rseries.
     * `anim_args`: A dictionary of arguments to pass to `matplotlib.animation.FuncAnimation`
     class.
     """
@@ -44,16 +49,26 @@ def animate_dataframes(frames: Iterable[pd.DataFrame], ax: plt.Axes,
         else:
             ylim = ((min1, max1),)
     elif isinstance(ylim, Iterable):
-        if len(ylim) == 2 and not all(map(lambda x: isinstance(x, Iterable), ylim)):
+        if len(ylim) == 2 and not any(map(lambda x: isinstance(x, Iterable), ylim)):
             ylim = (ylim, ylim)
+    
+    if isinstance(yscale, str): yscale = (yscale, yscale)
+    if isinstance(ylabel, str): ylabel = (ylabel, ylabel)
     
     if xlim is None:
         xlim = (min(f.index.values.min() for f in frames),
                 max(f.index.values.max() for f in frames))
-    
-    if not rseries:
-        ylabel = (ylabel,)
-        yscale = (yscale,)
+
+    if rseries:
+        if isinstance(fmt, str):
+            raise TypeError('Provide format string for rseries.')
+        elif isinstance(fmt, Iterable) and all((map(lambda x: isinstance(x, str), fmt))):
+            fmt = ((*fmt[:len(lseries)]), (*fmt[len(lseries):]))
+    else:
+        if isinstance(fmt, str):
+            fmt = [fmt] * len(lseries)
+        elif isinstance(fmt, Iterable) and all(map(lambda x: isinstance(x, Iterable), fmt)):
+            fmt = fmt[0] * len(lseries)
 
     # set up figure and axes
     fig = ax.get_figure()
@@ -65,8 +80,11 @@ def animate_dataframes(frames: Iterable[pd.DataFrame], ax: plt.Axes,
          ax2 = ax1.twinx()
          ax2.set(ylabel=ylabel[1], yscale=yscale[1], ylim=ylim[1])
 
-    lines1 = [ax1.plot([], [], label=s)[0] for s in lseries]
-    lines2 = [ax2.plot([], [], label=s)[0] for s in rseries]
+    lines1 = [ax1.plot([], [], f, label=s)[0] for s, f in zip_longest(lseries, fmt[0], fillvalue=fmt[0][-1])]
+    if rseries:
+        lines2 = [ax2.plot([], [], f, label=s)[0] for s, f in zip_longest(rseries, fmt[1], fillvalue=fmt[1][-1])]
+    else:
+        lines2 = []
 
     if legend:
         ax1.legend(lines1+lines2, (*lseries, *rseries), loc='upper right')
